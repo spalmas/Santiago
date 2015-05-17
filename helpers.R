3c2a8ba900d199840e14fdb542b1a8005cc576eb
spalmas <sebaspa@gmail.com>
2015-05-17 11:39:36 -0400

    Reduced number of classes

--- helpers.R
+++ helpers.R
@@ -19,6 +19,24 @@ biomass <- function(spp, dbh, h, crown){
     f = species[spp_i,'f_eq_biom']
     g = species[spp_i,'g_eq_biom']
     
+    
+    ######If statements to manage species exceptions
+    #The range of dbh vlaues for Schinus biomass eq is 8-45
+    if (spp_i == 'Schinus molle' & dbh_i > 45){ dbh_i = 45 }
+
+    #The range of dbh vlaues for Salix babylonica probaly does not reach 80. Dbh  changed to 40
+    if (spp_i == 'Salix babylonica' & dbh_i > 40){ dbh_i = 40 }
+    
+    #The range of dbh vlaues for citris limon probaly does not reach 156. Dbh changed to 15
+    if (spp_i == 'Citrus limon' & dbh_i > 15){ dbh_i = 15 }
+    
+    #The range of dbh vlaues for Prunus armeniaca probaly does not reach 134. Dbh changed to 25
+    if (spp_i == 'Prunus armeniaca' & dbh_i > 25){ dbh_i = 25 }
+    
+    #The range of dbh vlaues for Jacaranda mimosifolia probaly does not reach 166. Dbh changed to 20
+    if (spp_i == 'Jacaranda mimosifolia' & dbh_i > 20){ dbh_i = 20 }
+    
+    #######Biomass estimate
     if (biom_eq_type == 1){
       biomass_i[i] = exp(a + b * log(dbh_i))
       
@@ -32,17 +50,20 @@ biomass <- function(spp, dbh, h, crown){
       biomass_i[i] = a*h_i^b *  (dbh_i/c)^d
                          
     } else if (biom_eq_type == 5){
-      biomass_i[i] = a * dbh[i]^b + c * dbh[i]^d
+      biomass_i[i] = a * dbh_i^b + c * dbh_i^d
                          
     } else if (biom_eq_type == 6){
-      biomass_i[i] = exp(a + b * dbh[i])
+      biomass_i[i] = exp(a + b * dbh_i)
                          
     } else if (biom_eq_type == 7){
-      biomass_i[i] = a + b * log(dbh[i])
+      biomass_i[i] = a + b * log(dbh_i)
       
     } else if (biom_eq_type == 0){
       biomass_i[i] = 0
     }
+    
+    #Delete all trees with biomass larger than 2500, biomass eq biomass eq put of limits? measurement too big?
+    if (!is.na(biomass_i[i]) & biomass_i[i] > 2500){biomass_i[i] = 0} 
     
     i = i + 1
   }
